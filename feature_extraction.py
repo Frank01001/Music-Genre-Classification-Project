8748cb34abe127938c1be8d367628c0d2a3c73b9
Francesco Panebianco <xfrank01001@gmail.com>
2021-12-26 20:03:55 +0100

    Dataset generation complete

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,10 +1,54 @@
+import numpy as np
+import pandas as pd
+import os
+import wave
+
+
 def zero_crossing_rate(signal):
+    sign = np.sign(signal)
+    ZCR=0
+    for i in range(1, sign.size):
+        ZCR += np.absolute(sign[i] - sign[i-1])
+    return ZCR/(2*sign.size)
 
 
 def average_energy(signal):
+    return np.mean(signal ** 2)
 
 
-def silent_ration(signal):
+def silent_ratio(signal):
+    threshold = np.sqrt(average_energy(signal)) * 0.8
+    sr = 0
+    for i in range(signal.size):
+        if signal[i] < threshold:
+            sr += 1
+
+    return sr / signal.size
 
 
-    
+def create_dataset():
+    featurelist = list()
+    for root, dir, files in os.walk("./genres/"):
+        for k, genre in enumerate(dir):
+            print('Started genre {} ({})'.format(k, genre))
+            for root2, dir2, tracks in os.walk('./genres/' + genre + '/'):
+                for track in tracks:
+                    wav_file = wave.open(root2 + track, 'rb')
+                    signal = wav_file.readframes(-1)
+                    signal = np.frombuffer(signal, dtype='int16')
+                    signal = signal.astype('int32')
+                    wav_file.close()
+                    featurelist.append(zero_crossing_rate(signal))
+                    featurelist.append(average_energy(signal))
+                    featurelist.append(silent_ratio(signal))
+                    featurelist.append(k)
+                    print('Working on track {}'.format(track))
+                featurearr = np.array(featurelist).reshape(len(featurelist) // 4, 4)
+                df = pd.DataFrame(featurearr, columns=['ZCR', 'AVERAGE_ENERGY', 'SILENT_RATIO', 'CLASS'])
+                df.to_csv('test_dataset.csv')
+                return
+            print('Ended genre {} ({})'.format(k, genre))
+    featurearr = np.array(featurelist).reshape(len(featurelist)//4, 4)
+    df = pd.DataFrame(featurearr, columns=['ZCR', 'AVERAGE_ENERGY', 'SILENT_RATIO', 'CLASS'])
+    df.to_csv('test_dataset.csv')
+
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
+import feature_extraction
 
-test_file = wave.open('genres/classical/classical.00000.wav', 'rb')
+# feature_extraction.create_dataset()
+
+test_file = wave.open('genres/blues/blues.00000.wav', 'rb')
 # Extract Raw Audio from Wav File
 signal = test_file.readframes(-1)
 signal = np.frombuffer(signal, dtype='int16')
 test_file.close()
+
+# print(signal.shape)
 
 plt.figure(1)
 plt.title("Signal Wave...")
